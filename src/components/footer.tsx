0b3844411b2c01d7b52d534937f492006434d59d
abduganii <abduganiabdu2@gmail.com>
2024-12-03 11:04:16 +0500

    fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,12 +12,12 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   useGSAP(() => {
     gsap.fromTo(".Footer", {opacity:0,y:200}, { opacity: 1,x:0,y:0, duration: .7,delay:0.5, ease: "power2.out",scrollTrigger: {
       toggleActions: 'restart none none reset',
-      trigger: '.Footer',
+      trigger: '.Footermain',
     }})
  
   }); 
   return (
-    <Container className='mt-[24px]  overflow-hidden pb-[34px]' >
+    <Container className='mt-[24px] Footermain  overflow-hidden pb-[34px]' >
       <div className='bg-white w-full pt-[40px] Footer opacity-0  flex flex-wrap md:flex-nowrap items-start gap-[50px] lg:gap-[80px] xl:gap-[126px] px-[52px] pb-[66px] rounded-[42px]'>
         <Image src={'/logo.svg'} width={136} height={36} alt='img'/>
             <div className='ml-[20px]'>
