61ee1c254a9b8e848bb349efe97f01f3d470f222
abduganii <abduganiabdu2@gmail.com>
2024-12-04 12:00:11 +0500

    fixed

--- src/components/lang/lang/index.tsx
+++ src/components/lang/lang/index.tsx
@@ -6,22 +6,16 @@ import Image from 'next/image';
 import React from 'react'
 import { DownIcons } from '../../icons'
 
-import ru from "../../../../public/lang/ru.png"
-import uz from "../../../../public/lang/uz.png"
-import en from "../../../../public/lang/en.png"
 import {Link, usePathname} from '@/i18n/routing';
 import {  useLocale, useTranslations } from 'next-intl';
 import { LangArr } from '../../../../musk/data';
 
 
-// const langFlag:any = {
-//     ru:ru,
-//     uz:uz,
-//     en:en,
-// }
+
 export default function Lang() {
   const t = useTranslations('');
   const [open, setOpen] = useState(false)
+  const pathname = usePathname()
   return (
     <>
     <div className="relative">
@@ -32,20 +26,17 @@ export default function Lang() {
              {t('lang')}
             <DownIcons color='black'/>
           </div> 
-          {/* {
+          {
               open ? <ul className='max-w-[108px] absolute top-12  bg-[#0000001A] sm:py-3 rounded-xl w-full left-0"'>
                   {
                   LangArr.map((e) => (
-                      <Link href={pathname} className='flex font-normal text-base p-2   hover:bg-black' locale={e?.value} key={e.id}>
-                          <span className="px-2">
-                          <Image src={langFlag?.[e?.value]} alt='img' width={20} height={20}/>
-                          </span>
+                      <Link href={pathname} className='flex font-normal text-base p-2    hover:bg-white' locale={e?.value} key={e.id}>
                           {e.label}
                           </Link>
                       ))
                   }
               </ul>:""
-          } */}
+          }
       </div>
     </>
   )
